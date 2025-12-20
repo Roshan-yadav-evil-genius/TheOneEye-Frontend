9cda9cf89441529e7718dcc997ba3c7e36c43d8b
RoshanYadav <roshanyadav1st@gmail.com>
2025-12-20 07:04:25 +0000

    removed top right grreen dot execution indicator

--- src/components/features/workflow/custom-node.tsx
+++ src/components/features/workflow/custom-node.tsx
@@ -183,14 +183,6 @@ export function CustomNode({ id, data, selected, onDelete, workflowContext, isEx
           />
         )}
 
-        {/* Execution indicator */}
-        {isExecuting && (
-          <div className="absolute -top-1 -right-1 w-3 h-3">
-            <span className="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 animate-ping"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-          </div>
-        )}
-
         {/* Node Content - Icon or Loading Spinner */}
         <div className="flex items-center justify-center h-full">
           {isNodeExecuting ? (
